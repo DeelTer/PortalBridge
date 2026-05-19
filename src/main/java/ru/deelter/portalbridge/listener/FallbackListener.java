e35953b6e92ff9bf9ef289be0aff935ba3f26b58
Roman <roma200315@mail.ru>
2026-05-19 22:54:39 +0300

    Less messages

--- src/main/java/ru/deelter/portalbridge/listener/FallbackListener.java
+++ src/main/java/ru/deelter/portalbridge/listener/FallbackListener.java
@@ -4,9 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerKickEvent;
-import org.bukkit.event.server.PluginDisableEvent;
 import org.jspecify.annotations.NonNull;
 import ru.deelter.portalbridge.PortalBridgePlugin;
 import ru.deelter.portalbridge.config.ConfigManager;
@@ -14,7 +12,7 @@ import ru.deelter.portalbridge.config.ConfigManager;
 /**
  * Listens for player kicks (including bans) and redirects them to the fallback hub server
  * instead of disconnecting them, if {@code fallback.enabled} is true in config.
- *
+ * <p>
  * Runs at HIGHEST priority to act after other plugins have set their kick reasons.
  */
 @RequiredArgsConstructor
